55ae525c9a3622cafcdbd8c6e4ff08319ec92ffe
Harshit-Kumar <harshitkumarmaltumkar@gmail.com>
2026-01-09 01:35:58 -0800

    version_2

--- Demo.java
+++ Demo.java
@@ -3,5 +3,10 @@ public class Demo
     public static void main(String[] args)
     {
         System.out.println("Hello Wolrd!");
+        
+        int x=5, y=3, z;
+        z=x+y;
+        System.out.println(z);
+        
     }
 }
